fca592fc5a7554eb315ff236e7572ed6394f6975
The Daniel <dmnyc@users.noreply.github.com>
2025-11-09 23:18:08 -0500

    Update page title to include 'Your' for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Mutable - Nostr Mute List Manager",
+  title: "Mutable - Your Nostr Mute List Manager",
   description: "Your Nostr Mute List Manager",
   icons: {
     icon: '/mutable_logo.svg',
   },
   openGraph: {
-    title: "Mutable - Nostr Mute List Manager",
+    title: "Mutable - Your Nostr Mute List Manager",
     description: "Your Nostr Mute List Manager",
     images: ['/mutable_social_card.png'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: "Mutable - Nostr Mute List Manager",
+    title: "Mutable - Your Nostr Mute List Manager",
     description: "Your Nostr Mute List Manager",
     images: ['/mutable_social_card.png'],
   },
